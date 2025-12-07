6b20d95fb857c125e0479f8e8e9d14e4083583c3
Arunarasan <arunarasan06@gmail.com>
2025-12-07 11:40:37 +0530

    Complted

--- src/main/java/com/realestate/realestate/controller/AdminController.java
+++ src/main/java/com/realestate/realestate/controller/AdminController.java
@@ -66,7 +66,7 @@ public class AdminController {
 
         try {
             Admin saved = adminService.createAdmin(admin);
-            saved.setPassword(null);
+           
             return ResponseEntity.ok(saved);
 
         } catch (RuntimeException e) {
